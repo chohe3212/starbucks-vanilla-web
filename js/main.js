62ec3db7ff9c90b442c177851965a6c96830b3e3
chohe3212 <chohe3212@naver.com>
2023-03-22 23:43:41 +0900

    add visual

--- js/main.js
+++ js/main.js
@@ -21,3 +21,34 @@ searchinputEl.addEventListener('blur', function(){
     searchEl.classList.remove('focused');
     searchinputEl.setAttribute('placeholder',''); 
 });
+
+const badgeE1 = document.querySelector('header .badges');
+
+
+// scroll 이벤트를 할때 수십개의 이벤트가 발생하는데
+// 이렇게 되면 프로젝트가 무거워지고 안좋음...
+// 그래서 0.3초 단위로 부하를 줘서 이벤트 발생을 늦춤.
+// lodash에서 제공하는 throttle라는 특정한 기능을 사용함.
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // 광고 배찌 요소를 숨기기 
+        gsap.to(badgeE1, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+        // gsap.to(요소, 지속시간, 옵션);
+
+        // 스크롤을 내려도 클릭이 안되도록
+        // display 를 none으로
+
+    } else{
+        // 광고 배찌 보이기
+        gsap.to(badgeE1, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+},300));
+//_.throttle(함수, 시간);
+
