5ac90e8005a7e7794aebc8d7e3605a1a7e705a1b
chohe3212 <chohe3212@naver.com>
2023-03-28 18:29:56 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-const searchEl = document.querySelector(".search");
-const searchinputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener('click', function () {
-    searchinputEl.focus();
-} );
-
-
-// 검색창이 눌려지면 실행됨.
-searchinputEl.addEventListener('focus', function(){
-    // 해당 요소에 focused라는 클래스를 추가한다.
-    searchEl.classList.add('focused');
-
-    searchinputEl.setAttribute('placeholder','통합검색'); 
-    // 해당 요소에 어떤 html attribute를 부여할때 쓰는 함수
-    // 첫번째 인수 : 원하는 속성값 // 두번째 인수 : 해당 속성에 들어갈 값
-});
-
-// 검색창이 눌려지지 않았을때
-searchinputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchinputEl.setAttribute('placeholder',''); 
-});
-
 const badgeE1 = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -161,7 +137,3 @@ spyEls.forEach(function(spyEl) {
         .addTo(new ScrollMagic.Controller());
 
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2023이 나옴
-
